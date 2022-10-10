2383e947175eef134a75edeac9676414797f2d55
Artem Goreev <goreevartoum@gmail.com>
2022-10-11 01:19:53 +0300

    added ssl certificate

--- smtp/main.py
+++ smtp/main.py
@@ -1,20 +1,29 @@
-import sys
 import smtplib
-import hashlib
+import ssl
+import sys
 
 LOGIN = 'goreevartoum@gmail.com'
 PASSWORD = 'hcvjuxfdyvccdcdh'
+PORT = 587
+
+address = "artiom.goreev@yandex.ru"
+# text = input("[Template] Breaking bad!\n")
+message = """\
+Subject: Привет!
+[Template] Это письмо было отправлено из Python."""
 
 
 def main():
-    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-    smtpObj.starttls()
-    smtpObj.login(LOGIN, PASSWORD)
-
-    text = input("Breaking bad!\n")
-    address = "artiom.goreev@yandex.ru"
-
-    smtpObj.sendmail(LOGIN, address, text)
+    context = ssl.create_default_context()
+    # with smtplib.SMTP('smtp.gmail.com', PORT) as server:
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        try:
+            # server.starttls(context=context)
+            server.login(LOGIN, PASSWORD)
+            server.sendmail(LOGIN, address, message.encode())
+        except Exception as e:
+            # logging.log(2, e)
+            print("[Exception]", e)
 
 
 if __name__ == '__main__':
